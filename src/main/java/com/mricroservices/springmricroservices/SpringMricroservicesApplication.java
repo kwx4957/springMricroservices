57e69d01b6a66866f66b17e330266e0cc8cde9b4
dansam <33277725+kwx4957@users.noreply.github.com>
2022-01-11 23:16:05 +0900

    init

---
+++ src/main/java/com/mricroservices/springmricroservices/SpringMricroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mricroservices.springmricroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMricroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMricroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mricroservices/springmricroservices/SpringMricroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mricroservices.springmricroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMricroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
